355d3642759ca72d3837fae8655aa6d930bc3845
Miguel Chavez <toonchavez8@gmail.com>
2023-04-01 08:08:58 -0600

    install clerk and add signin button with middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+
 
 import { api } from "n/utils/api";
 
 import "n/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />
+  </ClerkProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
